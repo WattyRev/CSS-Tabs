db9d8fa5f14b954e08d004f88cb9edea04d7a806
Spencer Watson <spencer@wattydev.com>
2014-12-31 12:59:19 -0800

    Add state management

---
+++ state.jquery.js
@@ -0,0 +1,28 @@
+/**
+ * CSS-Tabs Plugin State Management
+ * Spencer Watson [wattydev.com]
+ * https://github.com/WattyRev/CSS-Tabs
+ */
+
+$(function() {
+	var q = window.location.search.replace('?', '').split('&');
+	var tab;
+	for (i = 0; i < q.length; i++) { 
+    	var query = q[i];
+    	if (query.split('=')[0] === 'tab') {
+    		tab = query.split('=')[1];
+    	}
+	}
+	$('#' + tab).attr('checked', 'checked');
+
+	$('input:radio').change(function() {
+		var radio = $(this);
+		if (!radio.parents('.tab-wrapper')) {
+			return;
+		}
+		var id = radio.attr('id');
+		var url = location.href.replace("tab="+tab, "tab="+id);
+		tab = id;
+		history.pushState(null, null, url);
+	});
+});
